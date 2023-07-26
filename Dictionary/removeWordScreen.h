ac266e2420250f23f265459395be544ca76c9d77
nghednh <nghednh@gmail.com>
2023-07-26 17:50:25 +0700

    removeWordScreen (same as addWordScreen with little modified)

--- Dictionary/removeWordScreen.h
+++ Dictionary/removeWordScreen.h
@@ -1,5 +1,5 @@
-//#pragma once
-//#include "Trie.h"
-//#include "MainScreen.h"
-//
-//void addNewWordScreen(RenderWindow& window, string typeDictionary, Trie& trie, Trie& favor_trie);
+#pragma once
+#include "Trie.h"
+#include "MainScreen.h"
+
+void removeWordScreen(RenderWindow& window, string typeDictionary, Trie& trie, Trie& favor_trie);
