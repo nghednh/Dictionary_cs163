a95694ebc7b0016d8ba2e842277b95dc7b84504d
thang2110 <137196567+thang2110@users.noreply.github.com>
2023-08-14 13:31:03 +0700

    Update removeWordScreen.h

--- Dictionary/removeWordScreen.h
+++ Dictionary/removeWordScreen.h
@@ -2,4 +2,4 @@
 #include "Trie.h"
 #include "MainScreen.h"
 
-void removeWordScreen(RenderWindow& window, string typeDictionary, Trie& trie, Trie& favor_trie);
+void removeWordScreen(RenderWindow& window, string typeDictionary, Trie& trie, Trie& favor_trie, Trie& history_trie);
