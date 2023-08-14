4919f94a7c1703f3f9eaa3e4bb7211d74dfda68a
thang2110 <137196567+thang2110@users.noreply.github.com>
2023-08-14 13:54:35 +0700

    Update search_saveFavorList.h

--- Dictionary/search_saveFavorList.h
+++ Dictionary/search_saveFavorList.h
@@ -5,7 +5,7 @@
 void wrapped_text(RectangleShape shape, Text& text);
 
 //-----------------Draw Scene--------------------------
-void search_addfavorite(RenderWindow& window, Trie &trie, string typeDictionary, Trie &favor_trie);
-void searchByDef(RenderWindow& window, Trie &trie, string typeDictionary, Trie &favor_trie);
+void search_addfavorite(RenderWindow& window, Trie& trie, string typeDictionary, Trie& favor_trie, Trie& history_trie);
+void searchByDef(RenderWindow& window, Trie& trie, string typeDictionary, Trie& favor_trie, Trie& history_trie);
 void display(TrieNode* root, string str, ofstream& fout);
 void findDef(TrieNode* root, string input, vector<pair<string, string>>& ans);
